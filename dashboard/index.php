be49cbb4c1672b1ee366ca81b8d163230e023070
diaszin <84644190+diaszin@users.noreply.github.com>
2023-11-16 23:09:12 -0300

    Importando boostrap

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Souza Cases - Dashboard</title>
+    <link rel="stylesheet" href="./bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <script src="./bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
